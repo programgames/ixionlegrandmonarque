9b85ea381191f70a0700dd818701894a67c17fbf
Julien Marcon <julgamer1@gmail.com>
2018-12-25 21:37:34 +0100

    Test spectator api

--- src/Controller/LoLController.php
+++ src/Controller/LoLController.php
@@ -13,18 +13,31 @@ class LoLController extends AbstractController
     public function index()
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLeSex?api_key=RGAPI-9976a438-0a4d-4e58-b31f-527b0c4fd9eb');
+        curl_setopt($ch, CURLOPT_URL, 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLanus?api_key=RGAPI-9976a438-0a4d-4e58-b31f-527b0c4fd9eb');
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $response = curl_exec($ch);
+        $response1 = curl_exec($ch);
         curl_close($ch);
 
-        $json = json_decode($response, true);
+        $json = json_decode($response1, true);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/ctvz1w1k5P77skErqL2KvwsQ8qQh3KlqT5rrp75n3EtlGmY?api_key=RGAPI-9976a438-0a4d-4e58-b31f-527b0c4fd9eb');
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response1 = curl_exec($ch);
+        curl_close($ch);
+
+        $json2 = json_decode($response1, true);
 
         return $this->render('lol/index.html.twig', [
             'controller_name' => 'LoLController',
-            'ixion' => $json['summonerLevel'],
+            'level' => $json['summonerLevel'],
+            'id' => $json['id'],
+            'gameMode' => $json2['gameMode'],
+            'gameLength' => ($json2['gameLength'] / 60 ) + 3
         ]);
     }
 }
