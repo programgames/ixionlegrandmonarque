9c99e0d45c0d1da4201a97f522a68346dc89e780
Julien Marcon <julgamer1@gmail.com>
2018-12-27 08:30:29 +0100

    updated lol twig and api key

--- src/LoLDataGetter/TeamMiamManager.php
+++ src/LoLDataGetter/TeamMiamManager.php
@@ -8,11 +8,11 @@ class TeamMiamManager {
     public function getTeamMiamInformations() : array {
 
         $teamMiam = array (
-          'tatas' => $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLanus?api_key=RGAPI-9976a438-0a4d-4e58-b31f-527b0c4fd9eb'),
-          'julien' =>  $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/marvin82?api_key=RGAPI-9976a438-0a4d-4e58-b31f-527b0c4fd9eb'),
-          'mela' =>  $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLaMeta?api_key=RGAPI-9976a438-0a4d-4e58-b31f-527b0c4fd9eb'),
-          'gwen' =>  $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLaBite?api_key=RGAPI-9976a438-0a4d-4e58-b31f-527b0c4fd9eb'),
-          'ixion' =>  $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLeSex?api_key=RGAPI-9976a438-0a4d-4e58-b31f-527b0c4fd9eb'),
+          'tatas' => $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLanus?api_key=RGAPI-f8aa8e4d-15be-465a-bed6-22874296460e'),
+          'julien' =>  $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/marvin82?api_key=RGAPI-f8aa8e4d-15be-465a-bed6-22874296460e'),
+          'mela' =>  $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLaMeta?api_key=RGAPI-f8aa8e4d-15be-465a-bed6-22874296460e'),
+          'gwen' =>  $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLaBite?api_key=RGAPI-f8aa8e4d-15be-465a-bed6-22874296460e'),
+          'ixion' =>  $this->urlGetRequestToArray('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MiamMiamLeSex?api_key=RGAPI-f8aa8e4d-15be-465a-bed6-22874296460e'),
 
         );
         return $teamMiam;
