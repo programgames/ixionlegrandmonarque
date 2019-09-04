e5ff136e33b7c5f6792d49285169c6a7451f1a6c
Julien Marcon <julgamer1@gmail.com>
2019-09-04 22:59:40 +0200

    update 2019 september

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
